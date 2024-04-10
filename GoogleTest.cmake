89c25b2e09c536ff866a83fbbcd0c563009c40d3
visuve <veli-matti.visuri@iki.fi>
2024-04-10 18:28:54 +0300

    Update gtest

--- GoogleTest.cmake
+++ GoogleTest.cmake
@@ -1,5 +1,8 @@
 include(FetchContent)
 
-FetchContent_Declare(googletest URL https://github.com/google/googletest/archive/refs/tags/release-1.12.1.zip)
+FetchContent_Declare(googletest
+	URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
+	URL_HASH SHA256=1f357c27ca988c3f7c6b4bf68a9395005ac6761f034046e9dde0896e3aba00e4
+)
 
 FetchContent_MakeAvailable(googletest)
